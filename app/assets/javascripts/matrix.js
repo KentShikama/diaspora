349b7147ebdd4e86c2128ea74c2d6ef2549d59db
Kent Shikama <kent@kentshikama.com>
2016-07-27 16:34:39 +0900

    Use timeout on sync for updating unread count

--- app/assets/javascripts/matrix.js
+++ app/assets/javascripts/matrix.js
@@ -3,33 +3,36 @@ var localStorage = window.localStorage;
 // potential TODO: matrix docs recommend creating a filter using the filter API for these situations
 // although the reason given is that sending the filter every time is too much overhead and ours is pretty short
 var filter = '{"event_fields":[],"account_data":{"types":[]},"presence":{"types":[]},' +
-    '"room":{"account_data":{"types":[]},"timeline":{"limit":1},"ephemeral":{"types":[]},"state":{"types":[]}}}';
-
+    '"room":{"account_data":{"types":[]},"timeline":{"types":[]},"ephemeral":{"types":[]},"state":{"types":[]}}}';
+var timeout = 30000; // amount of time the server will wait for an event before returning, in milliseconds
 // a function to update the unread messages displayed by the mail icon
-function updateUnreadCounter(counter, oldval) {
-  // find the counter elements if they are not passed as an argument
-  counter = typeof counter !== "undefined" ? counter : $(".unread-messages-counter");
-  oldval = typeof oldval !== "undefined" ? oldval : 0; // default old value is assumed to be 0
+// takes the html elements to be updated and a token telling the server when the last update was as arguments
+function updateUnreadCounter(counter, since_token) {
+// find the counter elements if they are not passed as an argument
+  counter = counter != undefined ? counter : $(".unread-messages-counter");
+// if the since token exists, make the corresponding URL parameter
+  var since_param = since_token != undefined ? "&since=" + since_token : "&full_state=true";
   var url = localStorage.getItem("mx_hs_url"); // homeserver address
   var token = localStorage.getItem("mx_access_token"); // user's access token
 
-  // if the necessary ingredients are lacking, give it a few seconds and try again
+// if the necessary ingredients are lacking, give it a few seconds and try again
   if (counter.length === 0 || url == null || token == null) {
     setTimeout(updateUnreadCounter, 5000);
     return;
   }
 
-  // get /_matrix/client/r0/sync from the homeserver, then count all unread notifications and update the page
-  $.getJSON(url + "/_matrix/client/r0/sync?access_token=" + token + "&filter=" + filter, function(result, status) {
+// get /_matrix/client/r0/sync from the homeserver, then count all unread notifications and update the page
+  $.getJSON(url + "/_matrix/client/r0/sync?access_token=" + token + "&filter=" + filter + "&timeout=" + timeout +
+      since_param, function(result, status) {
     if (status === "success") {
-      var counterVal = 0; // notification count
-      $.each(result.rooms.join, function(name, room) { // for each room the user has joined
-        if (room.unread_notifications.notification_count != undefined) { // which has a notification count
-          counterVal += room.unread_notifications.notification_count; // add to the total count
-        }
-      });
+      if (!_.isEmpty(result.rooms.join)) {
+        var counterVal = 0; // notification count
+        $.each(result.rooms.join, function(name, room) { // for each room the user has joined
+          if (room.unread_notifications.notification_count != undefined) { // which has a notification count
+            counterVal += room.unread_notifications.notification_count; // add to the total count
+          }
+        });
 
-      if (oldval !== counterVal) { // update page if necessary
         counter.text(counterVal.toString()); // update counter to reflect new value
         if (counterVal === 0) {
           counter.addClass("hidden"); // hide counter if no messages
@@ -39,8 +42,8 @@ function updateUnreadCounter(counter, oldval) {
       }
     }
 
-    // wait the specified number of milliseconds, then refresh the counter
-    setTimeout(function() { updateUnreadCounter(counter, counterVal); }, 1000);
+    // immediately poll the server again
+    updateUnreadCounter(counter, result.next_batch);
   });
 }
 
